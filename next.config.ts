c573589f3f6174663caa6dc23b5065b6da8850a3
saadexotic786-sudo <saadexotic786@gmail.com>
2026-08-30 00:42:00 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  images: { remotePatterns: [] },
-  experimental: { typedRoutes: true }
-};
-
-export default nextConfig;
